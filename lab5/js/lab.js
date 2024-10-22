c4654a5a8fb38a1d2422254001853ba2a2fe82eb
atrspenc <atrspenc@ucsc.edu>
2024-10-21 20:54:48 -0700

    MAJOR

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,12 +1,26 @@
-// index.js - Lab 
+// index.js - Lab 5
 // Author: Athena Spencer <atrspenc@ucsc.edu>
 // Date: 21 October
 
 // Define Variables
+make = "Volkswagen";
+model = "Jetta";
+color = "Silver";
+year = 2012;
 
-// Constants
+// Calculations
+currentYear = 2024
+age = currentYear - year;
 
-// Functions
+//Output
+function main() {
+  console.log("Main function started.");
+  document.writeln("Make: " + make + "<br>")
+  document.writeln("Model: " + model + "<br>")
+  document.writeln("Color: " + color + "<br>")
+  document.writeln("Age: " + age + "years<br>")
+  // the code that makes everything happen
+}
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -14,10 +28,6 @@ function myFunction(param1, param2) {
   // return results;
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
 // let's get this party started
 main();
