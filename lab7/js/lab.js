b209acb4ed5f5ec3ad187d5f817e635a553b6d84
atrspenc <atrspenc@ucsc.edu>
2024-10-28 18:16:03 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,8 +3,16 @@
 // Date: 28 October 2024
 
 
-// Create a new function in your file, call it what you like, but choose wisely.
-// In the function, declare a variable userName and use window.prompt() to get the user's name from the user. Here's some helpLinks to an external site..
+// In the function, declare a variable userName and use window.prompt() to get the user's name from the user. 
+// Variables
+function sortUserName() {
+  var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+  console.log("userName =", userName) ;
+  // split string to array
+  var nameArray = userName.split('') ;
+  console.log("nameArray =", nameArraysort) ;
+}
+
 // Sort the letters of the user's name and return those from the function. Hint: You can't sort strings, but you can sort arrays. Can you convert it back and forth?
 // Make sure your function uses return to return the results.
 // Outside of the function, call the function and output the results with document.writeln() to neatly output the user's sorted name.
