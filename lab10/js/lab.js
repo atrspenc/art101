7db31d30d4bbbed191096f18bc9aeda37b716898
atrspenc <atrspenc@ucsc.edu>
2024-11-05 14:27:56 -0800

    Update lab.js

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,3 +1,15 @@
 // index.js - Lab 10 JavaScript for the Web
 // Author: Athena Spencer
 // Date: 7 November 2024
+
+//create a little helper function that will generate fake dialogue
+function generateRandomText(){
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    //get a random starting index to slice the Loren Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    //generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+}
