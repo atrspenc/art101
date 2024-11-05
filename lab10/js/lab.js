85c90dccd0c4520a1c3d3794b8b0332435f4bb2b
atrspenc <atrspenc@ucsc.edu>
2024-11-05 14:38:42 -0800

    Update lab.js

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -7,9 +7,9 @@ function generateRandomText(){
     const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
     const min = 3;
     const max = 100;
-    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    const randLen = Math.floor(Math.random()*(max - min + 1)) + min;
     //get a random starting index to slice the Loren Ipsum text
-    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    const randStart = Math.floor(Math.random()*(text.length - randLen + 1));
     //generate the random Lorem Ipsum-like text
     return text.slice(randStart, randStart + randLen);
 }
