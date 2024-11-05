278c0b85cd9c002276e6824c68a080981eab3a82
atrspenc <atrspenc@ucsc.edu>
2024-11-04 17:43:44 -0800

    Update lab.js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,3 +1,12 @@
 // index.js - Lab 9 Libraries and jQuery
 // Author: Athena Spencer
 // Date: 4 November 2024
+
+// add button to challenge section
+$("#challenge").append("<button id='button challenge'>Make Special</button>");
+
+// add a click listener to the challenge button
+$("button-challenge").click(function()){
+    //now add (or subtract) the "special" class to the section
+    $("#challenge").toggleClass("special");
+}
