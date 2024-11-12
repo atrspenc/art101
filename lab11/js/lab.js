ebd97cc3d45e1c5d3a6f6ff5dc3fc5c2c8a73f06
atrspenc <atrspenc@ucsc.edu>
2024-11-11 21:58:35 -0800

    Update lab.js

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -4,31 +4,24 @@
     Requirements: jQuery must be loaded for this script to work.
 
     Author: Athena Spencer
-    Date: 11/7/24
+    Date: 11/11/24
 */
 
-//create a little helper function that will generate fake dialogue
-function generateRandomText() {
-    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    const min = 3;
-    const max = 100;
-    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-    // Get a random starting index to slice the Lorem Ipsum text
-    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-    // Generate the random Lorem Ipsum-like text
-    return text.slice(randStart, randStart + randLen);
-  }
+// Sorts the characters of a string in alphabetical order
+function sortString(inputString) {
+    // We have to convert our string to an array and back again to sort it
+    return inputString.split('').sort().join('');
+}
 
-  // get new fake dialogue
-const newText = generateRandomText();
-
-//add an event listener for your button
 // click listener for button
-$("#make-convo").click(function(){
-
-    //generate new random dialogue
-    const newText = generateRandomText();
-
-    // append a new div to our output div
-    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+$("#submit").click(function(){
+    //get value of input field
+    const userName = $("#user-name").val();
 });
+
+//sort name
+userNameSorted = sortString(userName);
+
+//append a new div to our output div
+$("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
+
