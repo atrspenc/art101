b0eeb0567935ffc1171c6293c16c4c26258fefe2
atrspenc <atrspenc@ucsc.edu>
2024-11-18 18:02:04 -0800

    loop 3 5 7

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,7 +7,19 @@
     Date: 11/18/24
 */
 
+//Loop through numbers 1 to 200, listing them as you go
+for(i = 1; i <= 200; i++) {
+    //If the number is a multiple of 3, if should print "Fizz!"
+    if (i % 3 == 0) {
+        console.log("Fizz!");
+    //If the number is a multiple of 5, it should print "Buzz!"
+    } else if (i % 5 == 0) {
+        console.log("Buzz!");
+    //If the number is a multiple of 7, it should print "Boom!"
+    } else if (i % 7 == 0) {
+        console.log("Boom!")
+    }
+}
 
 
-
-
+//If the number is a multiple of more than one 3, 5 or 7, it should combine Fizz, Buzz, and Boom. For example, if the number is a multiple of 3 AND 5, it should print "FizzBuzz!" and so on.
