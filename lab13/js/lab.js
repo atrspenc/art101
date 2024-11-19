9fa9ce8558a8339a691583a8911ea9c6fdaebbf2
atrspenc <atrspenc@ucsc.edu>
2024-11-18 17:56:58 -0800

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,31 +7,7 @@
     Date: 11/18/24
 */
 
-function fizzBuzzBoom() {
-    var outputStr = "";
-    for (var num = 1; num <= 200; num++) {
-      var str = "";
-      if (num % 3 == 0) str += "Fizz";
-      if (num % 5 == 0) str += "Buzz";
-      if (num % 7 == 0) str += "Boom";
-      var num = 15;
-      var str = "";
-      if (num % 3 == 0) {
-          str += "Fizz";
-      } if (num %5 == 0) {
-         str += "Buzz";
-      }
-      
-      console.log(str);
-  
-      outputStr += "<p>" + num + " " + str + "</p>\n"
-      
-     // or use this line instead:
-     // $("#output").append("<div>" + num + ": "+str+"</div>");
-  }
-  
-  fizzBuzzBoom();
-}
+
 
 
 
