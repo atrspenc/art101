82bf4e98d64e1251007e4668f9eae7ba9ddda83d
atrspenc <atrspenc@ucsc.edu>
2024-11-18 17:55:17 -0800

    Update lab.js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,30 +7,31 @@
     Date: 11/18/24
 */
 
-//Create a "FizzBuzz" function similar to how we did in class.
-//Loop through numbers 1 to 200, listing them as you go
-for (i=1; i <= 200; i++) {
-    //If the number is a multiple of 3, if should print "Fizz!"
-    if (i%3 == 0) {
-        console.log("Fizz!");
-    } //If the number is a multiple of 5, it should print "Buzz!"
-    else if (i%5 == 0) {
-        console.log("Buzz!");
-    }
-    //If the number is a multiple of 7, it should print "Boom!"
-    else if (i%7 == 0) {
-        console.log("Boom!");
-    }
-    //If the number is a multiple of more than one 3, 5 or 7, it should combine Fizz, Buzz, and Boom. For example, if the number is a multiple of 3 AND 5, it should print "FizzBuzz!" and so on.
-    else if (i%15 == 0) {
-        console.log("FizzBuzz!");
-    }
-    else if (i%21 == 0) {
-        console.log("FizzBoom!");
-    }
-    else if (i%35 == 0) {
-        console.log("BuzzBoom!");
-    }
+function fizzBuzzBoom() {
+    var outputStr = "";
+    for (var num = 1; num <= 200; num++) {
+      var str = "";
+      if (num % 3 == 0) str += "Fizz";
+      if (num % 5 == 0) str += "Buzz";
+      if (num % 7 == 0) str += "Boom";
+      var num = 15;
+      var str = "";
+      if (num % 3 == 0) {
+          str += "Fizz";
+      } if (num %5 == 0) {
+         str += "Buzz";
+      }
+      
+      console.log(str);
+  
+      outputStr += "<p>" + num + " " + str + "</p>\n"
+      
+     // or use this line instead:
+     // $("#output").append("<div>" + num + ": "+str+"</div>");
+  }
+  
+  fizzBuzzBoom();
 }
+
 
 
