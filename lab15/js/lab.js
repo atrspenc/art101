c0071f55ab5d7c3d0bba6180e8f11e6fdf1e773b
atrspenc <atrspenc@ucsc.edu>
2024-11-25 23:29:06 -0800

    reset

---
+++ lab15/js/lab.js
@@ -0,0 +1,8 @@
+/*
+    lab.js - This simple JavaScript/jQuery script appends new elements to an output div
+
+    Requirements: jQuery must be loaded for this script to work.
+
+    Author: Athena Spencer
+    Date: 11/21/24
+*/
